c2ed4d28d55b51e20fec7bfe0141ac384a1820bc
Platycraft <davidlavoy@davids-mbp.gateway.pace.com>
2014-05-27 11:32:32 -0500

    First Draft for KitPvp
    Still alot of work to do

---
+++ src/me/platycraft/listeners/PlayerListeners.java
@@ -0,0 +1,79 @@
+package me.platycraft.listeners;
+
+import me.platycraft.main.Main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class PlayerListeners implements Listener {
+	
+	Main plugin;
+	public PlayerListeners(Main instance){
+		this.plugin = instance;
+	}
+	
+	@EventHandler
+	public void PlayerJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		String pname = p.getName();
+		p.getInventory().clear();
+		//Message to send the server on join!
+		Bukkit.broadcastMessage("" + ChatColor.AQUA + ChatColor.BOLD + pname + ChatColor.RESET + ChatColor.GOLD + " has join the KIT PVP server!");
+	    
+		//Giving the player the Kit Selector
+		ItemStack kitselector = new ItemStack(Material.COMPASS);
+		ItemMeta m = kitselector.getItemMeta();
+		m.setDisplayName("" + ChatColor.GOLD + ChatColor.BOLD + "KIT SELECTOR!");
+		kitselector.setItemMeta(m);
+		p.getInventory().setItem(4,kitselector);
+	}
+	// End of Player joining the server
+	
+	//When Player Leave/quits the Server 
+	
+	
+    @EventHandler
+    public void PlayerLeave(PlayerQuitEvent e){
+    	Player p = e.getPlayer();
+		String pname = p.getName();
+		p.getInventory().clear();
+		//Message to send the server on Leave!
+		Bukkit.broadcastMessage("" + ChatColor.AQUA + ChatColor.BOLD + pname + ChatColor.RESET + ChatColor.GOLD + " has left the KIT PVP server!");
+    }
+    //End of player Leaving the server 
+    
+    @EventHandler
+    public void Death(PlayerDeathEvent e){
+    	
+    	Player p = e.getEntity();
+    	Player k = p.getKiller();
+    	e.getDrops().clear();
+    	e.setDeathMessage("" + ChatColor.BLUE + e.getEntity() + ChatColor.AQUA + "was killed by" + ChatColor.RED + p.getKiller());
+    	p.sendMessage(ChatColor.AQUA + "Your death was by" + ChatColor.RED + k.getName());
+    	
+    }
+    
+    @EventHandler
+    public void Respawn(PlayerRespawnEvent e){
+    	Player p = e.getPlayer();
+    	
+    	//Gives  Inventory 
+    	ItemStack kitselector = new ItemStack(Material.COMPASS);
+		ItemMeta m = kitselector.getItemMeta();
+		m.setDisplayName("" + ChatColor.GOLD + ChatColor.BOLD + "KIT SELECTOR!");
+		kitselector.setItemMeta(m);
+		p.getInventory().setItem(4,kitselector);
+    	
+    }
+
+}
---
+++ src/me/platycraft/main/Main.java
@@ -0,0 +1,127 @@
+package me.platycraft.main;
+
+import me.platycraft.listeners.PlayerListeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	private static Main instance;
+	
+	public void log(String string){
+		System.out.println(string);
+	}
+	
+	public static Main getInstance(){
+		return instance; 
+	}
+	
+	public void registerListeners(){
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new PlayerListeners(this), this);
+	}
+	
+	public void registerCommands(){
+		//Not using this just yet
+	}
+	
+	@Override
+	public void  onEnable(){
+		getLogger().info("has been enabled successfully!");
+		instance = this;
+		registerListeners();
+		registerCommands();
+	}
+	
+	@Override
+	public void onDisable(){
+		getLogger().info("has been disabled successfully!");
+		instance = null;
+	}
+	
+	//COMMANDS 
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		
+		//Starter Command
+		
+		if (cmd.getName().equalsIgnoreCase("starter") && sender instanceof Player){
+			
+			Player p = (Player) sender;
+			p.getInventory().clear();
+			ItemStack starterbow = new ItemStack(Material.BOW);
+			ItemMeta m = starterbow.getItemMeta();
+			m.setDisplayName("" + ChatColor.GREEN + ChatColor.BOLD + "STARTER BOW!");
+			starterbow.setItemMeta(m);
+			starterbow.addEnchantment(Enchantment.DURABILITY, 1);
+			starterbow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+			p.getInventory().addItem(starterbow);
+			p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
+			p.getInventory().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+			p.getInventory().setLeggings(new ItemStack(Material.LEATHER_BOOTS));
+			p.setMaxHealth(30);
+			p.setHealth(30);
+			p.sendMessage(ChatColor.AQUA + "You was given the" + ChatColor.BOLD + ChatColor.ITALIC + ChatColor.GREEN + "Starter Bow");
+			
+			
+		}
+		// End of start command
+		
+		//Amature Command 
+		
+if (cmd.getName().equalsIgnoreCase("amateur") && sender instanceof Player){
+			
+			Player p = (Player) sender;
+			p.getInventory().clear();
+			ItemStack amateurbow = new ItemStack(Material.BOW);
+			ItemMeta m = amateurbow.getItemMeta();
+			m.setDisplayName("" + ChatColor.YELLOW + ChatColor.BOLD + "AMATEUR BOW!");
+			amateurbow.setItemMeta(m);
+			amateurbow.addEnchantment(Enchantment.DURABILITY, 1);
+			amateurbow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+			p.getInventory().addItem(amateurbow);
+			p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
+			p.getInventory().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+			p.getInventory().setLeggings(new ItemStack(Material.LEATHER_BOOTS));
+			p.setMaxHealth(30);
+			p.setHealth(30);
+			p.sendMessage(ChatColor.AQUA + "You was given the" + ChatColor.BOLD + ChatColor.ITALIC + ChatColor.GREEN + "Starter KIT !");
+			ItemStack amateursword = new ItemStack(Material.WOOD_SWORD);
+			ItemMeta n = amateursword.getItemMeta();
+			n.setDisplayName("" + ChatColor.YELLOW + ChatColor.BOLD + "AMATUER BOW!");
+			amateursword.setItemMeta(n);
+			amateursword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+			amateursword.addEnchantment(Enchantment.KNOCKBACK, 1);
+			p.getInventory().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+			p.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS));
+			p.getInventory().setHelmet(new ItemStack(Material.LEATHER_HELMET));
+			p.getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+			p.getInventory().addItem(amateursword);
+			p.getInventory().addItem(amateurbow);
+			p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
+			p.sendMessage(ChatColor.AQUA + "You was given the" + ChatColor.BOLD + ChatColor.ITALIC + ChatColor.YELLOW + "Amateur KIT !");
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		return false;		
+	}
+	
+	
+	
+	
+}
